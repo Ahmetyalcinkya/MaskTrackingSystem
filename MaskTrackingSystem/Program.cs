c940c78089a175e310b24e15c07befbca0ea1f18
Ahmetyalcinkya <ahmetcan.yalcinkaya55@gmail.com>
2024-02-07 04:08:44 +0300

    Proje dosyası ekle.

---
+++ MaskTrackingSystem/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Entities.Concretes;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Program created;");
+
+        Person person1 = new Person();
+
+        person1.FirstName = "Test1";
+        person1.LastName = "Test1";
+        person1.NationalIdentity = 11111111111;
+        person1.DateOfBirthYear = 1998;
+
+        Person person2 = new Person();
+
+        person2.FirstName = "Test2";
+        person2.LastName = "Test1";
+        person2.NationalIdentity = 22222222222;
+        person2.DateOfBirthYear = 1999;
+
+        Person person3 = new Person();
+
+        person3.FirstName = "Test1";
+        person3.LastName = "Test1";
+        person3.NationalIdentity = 33333333333;
+        person3.DateOfBirthYear = 2000;
+
+    }
+}
